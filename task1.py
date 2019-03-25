a584c0f99e75edbca921f3a055b16910ca7816bd
Will <student@ssu-cs-dar28-167-218.sonoma.edu>
2019-03-25 16:56:54 -0700

    Made changes to deque_add for subtraction, added borrowing functionality. Needs more work.

--- task1.py
+++ task1.py
@@ -12,16 +12,34 @@ def main():
 
 
 def deque_add(a, b, neg_b):
+    if neg_b:
+        k = 0
+        while k < len(a) and a[k] == b[k]:
+            k += 1
+        bLess = b[k] < a[k]
     ret_deque = deque()
 
     carry = 0
-    for i in range(len(a - 1)):
+    for i in range(len(a) - 1, -1, -1):
+        cur_a = int(a[i])
         curr_b = int(b[i])
         if neg_b:
             curr_b *= -1
+            if abs(cur_b) > a[i]:
+                curr_a += 10
 
-        carry = string(int(a[i]) + curr_b + carry)
-        str_carry = string(carry)
+                j = i
+                while j > 0 and a[j] == 0:
+                    a[j] = 9
+                    j -= 1
+                if j != 0 or a[0] >= b[0]:
+                    a[0] -= 1
+                    curr_
+                else:
+                    ret_deque[0] = curr_b - a[0]
+
+        carry = int(a[i]) + curr_b + carry
+        str_carry = str(carry)
         if carry < 0:
             comp_len = 2
             str_carry = str_carry[1:]
@@ -31,16 +49,18 @@ def deque_add(a, b, neg_b):
             sign = ""
 
         if len(str_carry) > comp_len:
-            ret_deque.prepend(int(str_carry[1]))
+            ret_deque.appendleft(int(str_carry[1]))
             carry = int(sign + str_carry[0])
         else:
-            ret_deque.prepend(abs(carry))
+            ret_deque.appendleft(abs(carry))
             carry = 0
 
     if int(carry) > 0:
-        ret_deque.prepend(int(carry))
+        ret_deque.appendleft(int(carry))
+    if neg_b:
+        ret_deque.appendleft('-')
 
-    return ret_list
+    return ret_deque
 
 
 def karatsuba_mult(a, b):
@@ -89,3 +109,15 @@ def powtest():
     print(num)
 
 
+def dequeaddtest():
+    a = input("Enter value1 for deq add: ")
+    b = input("Enter value2 for deq add: ")
+    addorsub = input("subtraction?")
+    while a or b != 'q':
+        print(deque_add(deque(a), deque(b), addorsub == 't'))
+        a = input("Enter value1 for deq add: ")
+        b = input("Enter value2 for deq add: ")
+        addorsub = input("subtraction?")
+
+
+dequeaddtest()
