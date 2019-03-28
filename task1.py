6b25b3c93c45a26f453a9d918ae281b4ac292fd2
supersaiyansubtlety <supersaiyansubtlety@users.noreply.github.com>
2019-03-27 21:03:53 -0700

    added comments for clarity and cleaned up some things

--- task1.py
+++ task1.py
@@ -5,50 +5,58 @@ from itertools import islice
 
 def main():
     print("Running Karatsubas large interger multiplication")
+    # get input, convert to deques(single-char strings)
     a = deque(input("Enter the value for the first integer: "))
     b = deque(input("Enter the value for the second integer: "))
 
+    # convert deques of strings to deques of ints
     for i in range(len(a)):
         a[i] = int(a[i])
     for i in range(len(b)):
         b[i] = int(b[i])
 
+    # get and print result
     print("The product is: ", "".join(map(str, karatsuba_mult(a, b))))
 
 
+# function that transforms input for use in deque_arr_add/sub
+# expects two deques of ints, allowing for '-' as first element and differing lengths
 def deque_arr(a, b, sub):
+    # store and strip negative for a
     a_neg = False
     if a[0] == '-':
         del a[0]
         a_neg = True
 
+    # store and strip negative for b
     b_neg = False
     if b[0] == '-':
         del b[0]
         b_neg = True
 
+    # append 0's so len(a) == len(b)
     while len(a) > len(b):
         b.appendleft(0)
     while len(b) > len(a):
         a.appendleft(0)
 
+    # determine if abs(b) < abs(a), as deque_arr_add/sub expect abs(a) >= abs(b)
     k = 0
     while k < len(a) - 1 and a[k] == b[k]:
         k += 1
-    bAbsLess = b[k] < a[k]
+    b_abs_less = b[k] < a[k]
 
     # make b positive
     if b_neg:
         b_neg = not b_neg
         sub = not sub
 
-    #if subtracting large, positive b
-    if (not bAbsLess) and sub:
+    # if subtracting large positive b, swap
+    if (not b_abs_less) and sub:
         # form is (+/-a) - (+B)
         # change this to:
         #    (-B) + (+/-a)
         sub = not sub
-        #a_neg = not a_neg
         b_neg = not b_neg
         swap = b
         b = a
@@ -56,23 +64,36 @@ def deque_arr(a, b, sub):
         swap = a_neg
         a_neg = b_neg
         b_neg = swap
+    # now we know abs(a) >= abs(b)
 
-    ##now we know abs(a) >= abs(b)
-    ##### find deque op
+    # find deque op
+    #         (-a) + (+b)                OR        (-a) - (-b)
     if (a_neg and not b_neg and not sub) or (a_neg and b_neg and sub):
-        ret_deque = deque_sub(a, b)
+        # =>    -1 * ((a) - (b))
+        ret_deque = deque_arr_sub(a, b)
         ret_deque.appendleft('-')
         return ret_deque
+
+    #               (+a) + (+b)                OR       (+a) - (-b)
     elif (not a_neg and not b_neg and not sub) or (not a_neg and b_neg and sub):
-        return deque_add(a, b)
+        # =>    (a) + (b)
+        return deque_arr_add(a, b)
+
+    #               (+a) + (-b)            OR           (+a) - (+b)
     elif (not a_neg and b_neg and not sub) or (not a_neg and not b_neg and sub):
-        return deque_sub(a, b)
+        # =>    (a) - (b)
+        return deque_arr_sub(a, b)
+
+    #               (-a) + (-b)        OR           (-a) - (+b)
     elif (a_neg and b_neg and not sub) or (a_neg and not b_neg and sub):
-        ret_deque = (deque_add(a, b).appendleft('-'))
+        # =>    -1 * ((a) + (b))
+        ret_deque = (deque_arr_add(a, b).appendleft('-'))
         return ret_deque
 
-def deque_add(a, b):
-    #expects two deques of ints, both positive, with a >= b
+
+# performs simple deque addition
+# expects two deques of ints, both positive, with a >= b
+def deque_arr_add(a, b):
     ret_deque = deque()
 
     carry = 0
@@ -93,7 +114,9 @@ def deque_add(a, b):
     return ret_deque
 
 
-def deque_sub(a, b):
+# performs simple deque subtraction
+# expects two deques of ints, both positive, with a >= b
+def deque_arr_sub(a, b):
     ret_deque = deque()
 
     for i in range(len(a) - 1, -1, -1):
@@ -112,7 +135,20 @@ def deque_sub(a, b):
     return ret_deque
 
 
+# wrapper for deque_arr addition
+def deque_add(a, b):
+    return deque_arr(a, b, False)
+
+
+# wrapper for deque_arr subtraction
+def deque_sub(a, b):
+    return deque_arr(a, b, True)
+
+
+# fast multiplication algorithm
+# expects two deques of ints, allowing for '-' as first element and differing lengths
 def karatsuba_mult(a, b):
+    # stip negatives and record what the sign of the return should be
     ret_neg = False
     if a[0] == '-':
         ret_neg = not ret_neg
@@ -121,21 +157,26 @@ def karatsuba_mult(a, b):
         ret_neg = not ret_neg
         del b[0]
 
+    # append 0's so len(a) == len(b)
     while len(a) > len(b):
         b.appendleft(0)
     while len(b) > len(a):
         a.appendleft(0)
 
+    # base case: a and b are both single-digit
     if len(a) == 1 and len(b) == 1:
+        # cast result of multiplication to string so digits are separated, convert string to deque of single-char strings
         ret_deque = deque(str(a[0] * b[0]))
+        # convert deque of strings to deque of ints
         for i in range(len(ret_deque)):
             ret_deque[i] = int(ret_deque[i])
+        # append negative if exactly one of the inputs was negative
         if ret_neg:
             ret_deque.appendleft('-')
+        # return the result
         return ret_deque
 
-
-    #A*B = A1*B1*10^2k+(C1)*10^k+A0*B0, A1 = K+1, A0 = k, K = floor(n/2)
+    # A*B = A1*B1*10^2k+(C1)*10^k+A0*B0, A1 = K+1, A0 = k, K = floor(n/2)
     k = floor(len(a)/2)
     if (len(a) % 2) == 0:
         a1 = deque(islice(a, 0, k))
@@ -151,7 +192,7 @@ def karatsuba_mult(a, b):
 
     c2 = karatsuba_mult(a1, b1)
     c0 = karatsuba_mult(a0, b0)
-    c1 = deque_arr(karatsuba_mult(deque_arr(a1, a0, False), deque_arr(b1, b0, False)), deque_arr(c2, c0, False), True)
+    c1 = deque_sub(karatsuba_mult(deque_add(a1, a0), deque_add(b1, b0)), deque_add(c2, c0))
 
     return pow10(c2, 2*k) + pow10(c1, k) + c0
 
@@ -205,5 +246,5 @@ def dequeaddtest():
         addorsub = input("subtraction?")
 
 
-#main()
-dequeaddtest()
+main()
+# dequeaddtest()
